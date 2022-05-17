a989eed70e79342e24df09e2eeb14733412146eb
dosmaster4life <jefferysuperman@gmail.com>
2022-05-17 12:27:58 -0600

    Journal Entry Feature

---
+++ lib/screens/MainTabController.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Entry.dart';
+import 'JournalList.dart';
+import 'Settings.dart';
+
+class MainTabController extends StatefulWidget {
+  const MainTabController({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MainTabController> createState() => _MainTabControllerState();
+}
+
+class _MainTabControllerState extends State<MainTabController> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(children: [
+          JournalList(),
+          Settings(),
+
+        ],
+
+        ),
+        appBar: AppBar(
+          toolbarHeight: 1,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Journal", icon: Icon(Icons.accessibility)),
+              Tab(text: "Settings",icon: Icon(Icons.settings)),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () { Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Entry(),
+            )); },
+
+        ),
+      ),
+    );
+  }
+}
